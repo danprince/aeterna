cee7a3583aa207b2fe11c6cc4377191ca0d06500
danprince <danielprince@hotmail.co.uk>
2016-03-03 12:23:26 +0530

    adds extra test for equals

--- test/map/equals.spec.js
+++ test/map/equals.spec.js
@@ -3,7 +3,7 @@ const Aeterna = require('../..');
 const Map = Aeterna.Map;
 
 test('equals', (t) => {
-  t.plan(6);
+  t.plan(7);
 
   const m = Map({ a: 1 });
   t.true(
@@ -31,6 +31,11 @@ test('equals', (t) => {
     Map({ a: Map({ b: 2, c: 3 }) })
       .equals(Map({ a: Map({ b: 2, c: 4 }) })),
     'should find inequality in nested structure'
+  );
+
+  t.false(
+    Map({ z: 1, y: 2 }).equals(Map({ z: 1, y: 2, x: 3 })),
+    'should find inequality with additional keys'
   );
 
   t.throws(
